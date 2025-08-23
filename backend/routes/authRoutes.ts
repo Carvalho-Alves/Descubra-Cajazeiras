37690839a755360f5351d689b0173ebe7274d76f
Carvalho-Alves <pedrocasa808080@gmail.com>
2025-08-23 00:08:15 -0300

    Consertando rotas

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 import { loginController } from '../controller/loginUser';
 import { registerController } from '../controller/createUser';
+import { editUser } from '../controller/editUser';
 import { findUser } from '../controller/findUser';
 import { getUsers } from '../controller/findUsers';
+import { deleteUser } from '../controller/deleteUser';
 import { asyncHandler } from '../utils/asyncHandler';
+import multer from 'multer';
 
 const router = Router();
+const upload = multer({ dest: 'backend/uploads/' }); // Pasta para uploads
 
 router.post('/login', asyncHandler(loginController));
-router.post('/register', asyncHandler(registerController));
 router.get('/:id', asyncHandler(findUser));
 router.get('/', asyncHandler(getUsers));
+router.post('/register', upload.single('foto'), asyncHandler(registerController));
+router.put('/:id', upload.single('foto'), asyncHandler(editUser));
+router.delete('/:id', asyncHandler(deleteUser));
+
+
 
 export default router;
