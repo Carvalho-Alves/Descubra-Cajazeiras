280429ca2de84f96b55ccc85197a21c92cfb389e
Carvalho-Alves <pedrocasa808080@gmail.com>
2025-08-22 22:46:03 -0300

    Adicionado controller para deletar usuario

---
+++ backend/controller/deleteUser.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+import { deleteUserService } from '../service/userService'; // Importa o serviço de delete
+
+export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  
+    const { id } = req.params;
+    const result = await deleteUserService(id);
+    return res.status(200).json(result);
+  };
