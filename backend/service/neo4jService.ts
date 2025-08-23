8d56085ad3c44f1eaa5dcffb4d8d282370891142
Carvalho-Alves <pedrocasa808080@gmail.com>
2025-08-22 22:59:09 -0300

    Adicionado serviços para interagir com o banco de dados neo4j, permitindo que crie e relacione nós

---
+++ backend/service/neo4jService.ts
@@ -0,0 +1,19 @@
+import { getNeo4jDriver } from '../database/neo4j';
+
+export async function criarNoLocal(localId: string, titulo: string) {
+  const session = getNeo4jDriver().session();
+    await session.run('CREATE (l:Local {id:$id, titulo:$titulo})', { id: localId, titulo });
+    await session.close();
+}
+
+export async function criarNoCategoria(categoriaId: string, nome: string) {
+  const session = getNeo4jDriver().session();
+    await session.run('CREATE (c:Categoria {id:$id, nome:$nome})', { id: categoriaId, nome });
+    await session.close();
+}
+
+export async function relacionarLocalCategoria(localId: string, categoriaId: string) {
+  const session = getNeo4jDriver().session();
+    await session.run('MATCH (l:Local {id:$localId}), (c:Categoria {id:$categoriaId}) MERGE (l)-[:PERTENCE_A]->(c)', { localId, categoriaId });
+    await session.close();
+}
